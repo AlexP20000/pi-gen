d4c5fd880837608436fcd9c5955e518248da15d8
vagrant <vagrant@debian10.localdomain>
2021-12-13 21:41:47 +0000

    Script d'ajout du repot pour Grafana

--- stage2-tocio/00-system/00-run.sh
+++ stage2-tocio/00-system/00-run.sh
@@ -1,5 +1,4 @@
 # Add Grafana repo
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-
 # Add the Grafana APT repository:
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
