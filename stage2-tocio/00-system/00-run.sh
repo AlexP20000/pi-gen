5292ebc5b15f54bd797aa751b74cc15e43575ff9
vagrant <vagrant@debian10.localdomain>
2021-12-17 15:19:48 +0000

    Configuration pour TOCIO

--- stage2-tocio/00-system/00-run.sh
+++ stage2-tocio/00-system/00-run.sh
@@ -1,4 +1,14 @@
+# Remove keyboard layout
+rm /etc/default/keyboard
+# insert custom keyboard layout (for french keyboards)
+echo "XKBMODEL='c105'" > /etc/default/keyboard
+echo "XKBLAYOUT='fr'" >> /etc/default/keyboard
+echo "XKBVARIANT='azerty'" >> /etc/default/keyboard
+echo "XKBOPTIONS=''" >> /etc/default/keyboard
+echo "BACKSPACE='guess'" >> /etc/default/keyboard
+
+
 # Add Grafana repo
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-# Add the Grafana APT repository:
+# Add the Grafana APT repository
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
